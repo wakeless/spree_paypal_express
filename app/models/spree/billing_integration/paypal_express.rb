0d75045042afbaaca97b95a0018fdff5ad0aab6b
Michael Gall <michael@wakeless.net>
2012-04-19 14:19:00 +1000

    Making attributes accessible

--- app/models/spree/billing_integration/paypal_express.rb
+++ app/models/spree/billing_integration/paypal_express.rb
@@ -1,4 +1,6 @@
 class Spree::BillingIntegration::PaypalExpress < Spree::BillingIntegration
+  attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_review, :preferred_no_shipping, :preferred_server, :preferred_test_mode, :preferred_currency, :preferred_allow_guest_checkout
+
   preference :login, :string
   preference :password, :password
   preference :signature, :string
